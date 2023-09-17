b71dc1028c1467b971d79f2a4226bf473b7bd2d8
DONERMETER <118672005+DONERMETER@users.noreply.github.com>
2023-09-17 19:15:22 +0200

    Update node.js

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 const app = express();
 const { exec } = require('child_process');
 
