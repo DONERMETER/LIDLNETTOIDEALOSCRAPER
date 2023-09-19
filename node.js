0a1c15df70496aa810bbed73fed712c884f6fcc3
DONERMETER <118672005+DONERMETER@users.noreply.github.com>
2023-09-19 17:08:31 +0200

    Update node.js

--- node.js
+++ node.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const app = express();
 const { exec } = require('child_process');
 
-// Dynamic port binding for Azure
+// Azure Dynamic Port Configuration
 const port = process.env.PORT || 8080;
 
 let chromiumVersion = 'Checking...';
 let puppeteerVersion = require('puppeteer-core/package.json').version;
 let nodeVersion = process.version;
 
-// Check Chromium version when the app starts
+// Validate Chromium Version at Startup
 exec('chromium-browser --version', (error, stdout, stderr) => {
   if (error) {
-    console.error(`Error executing Chromium command: ${error}`);
+    console.error(`Chromium Command Error: ${error}`);
     chromiumVersion = 'Error';
   } else {
-    console.log(`Chromium version: ${stdout}`);
+    console.log(`Chromium Version: ${stdout}`);
     chromiumVersion = stdout;
   }
 });
 
 app.get('/', (req, res) => {
-  res.send(`Scraping service is running.<br>
-            STATUS: <br>
-            PUPPETEER: ${puppeteerVersion} OK<br>
-            CHROMIUM: ${chromiumVersion}<br>
-            NODE: ${nodeVersion} OK`);
+  res.send(`Scraping Service Active.<br>
+            Status: <br>
+            Puppeteer Version: ${puppeteerVersion} OK<br>
+            Chromium Version: ${chromiumVersion}<br>
+            Node Version: ${nodeVersion} OK`);
 });
 
 app.get('/scrape', async (req, res) => {
@@ -44,7 +44,7 @@ app.get('/scrape', async (req, res) => {
       const seller = decodeURIComponent(req.query.seller);
       result = await scrapeIdealo(url, seller);
     } else {
-      return res.status(400).send({ error: 'Invalid store type' });
+      return res.status(400).send({ error: 'Invalid Store' });
     }
 
     res.send(result);
@@ -52,105 +52,73 @@ app.get('/scrape', async (req, res) => {
     res.status(500).send({ error: error.message });
   }
 });
-
-async function scrapeLidl(url, targetArticleNumber) {
-  const browser = await puppeteer.launch();
+async function fetchLidlData(url, targetArticleNumber) {
+  const browser = await puppeteer.launch({ executablePath: '/usr/bin/chromium-browser' });
   const page = await browser.newPage();
   await page.goto(url);
 
   await page.waitForSelector('.buybox__erp-number', { timeout: 10000 });
-  
-  const variationOptions = await page.$$eval('#product-P_ART option', options => options.map(o => o.value));
-  
-  for (const option of variationOptions) {
-    if (option) {
-      await page.select('#product-P_ART', option);
+
+  const optionsList = await page.$$eval('#product-P_ART option', options => options.map(option => option.value));
+
+  for (const opt of optionsList) {
+    if (opt) {
+      await page.select('#product-P_ART', opt);
       await page.waitForTimeout(2000);
       await page.waitForSelector('.m-price__price', { timeout: 2000 });
-      
-      const currentArticleNumber = await page.$eval('.buybox__erp-number', el => el.textContent.trim());
-      
-      if (currentArticleNumber === targetArticleNumber) {
-        const price = await page.$eval('.m-price__price', el => el.textContent.trim());
+
+      const currentArticle = await page.$eval('.buybox__erp-number', element => element.textContent.trim());
+
+      if (currentArticle === targetArticleNumber) {
+        const priceData = await page.$eval('.m-price__price', element => element.textContent.trim());
         await browser.close();
-        return [{ variation: option, price: parseFloat(price) }];
+        return [{ variation: opt, price: parseFloat(priceData) }];
       }
     }
   }
-  
   await browser.close();
-  return { error: 'No data found for the given parameters.' };
+  return { error: 'No matching data found.' };
 }
 
-async function scrapeNetto(url) {
-  const browser = await puppeteer.launch();
+async function fetchNettoData(url) {
+  const browser = await puppeteer.launch({ executablePath: '/usr/bin/chromium-browser' });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'load', timeout: 60000 });
 
-  const priceBeforeComma = await page.$eval('.prices__ins--digits-before-comma', el => el.textContent.trim());
-  const priceAfterComma = await page.$eval('.product__current-price--digits-after-comma', el => el.textContent.trim());
-  const price = parseFloat(priceBeforeComma + '.' + priceAfterComma);
+  const priceInt = await page.$eval('.prices__ins--digits-before-comma', element => element.textContent.trim());
+  const priceDec = await page.$eval('.product__current-price--digits-after-comma', element => element.textContent.trim());
+  const finalPrice = parseFloat(priceInt + '.' + priceDec);
 
   await browser.close();
-  return [{ price: price }];
+  return [{ price: finalPrice }];
 }
 
-async function scrapeIdealo(url, sellerName) {
-  const browser = await puppeteer.launch({ headless: false });
+async function fetchIdealoData(url, shopName) {
+  const browser = await puppeteer.launch({ executablePath: '/usr/bin/chromium-browser', headless: false });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
 
-  const offers = await page.$$eval('.productOffers-listItemOfferLink', (offers, sellerName) => {
-    const results = [];
+  const offersList = await page.$$eval('.productOffers-listItemOfferLink', (offers, shopName) => {
+    const resultsArray = [];
     for (const offer of offers) {
-      const shopName = offer.getAttribute('data-shop-name');
-      let price = null;
+      const storeName = offer.getAttribute('data-shop-name');
+      let offerPrice = null;
+
       try {
         const payload = JSON.parse(offer.getAttribute('data-gtm-payload').replace(/&quot;/g, '"'));
-        price = parseFloat(payload.product_price);
+        offerPrice = parseFloat(payload.product_price);
       } catch (error) {
-        console.error("Error parsing price: ", error);
+        console.error("Error parsing the price: ", error);
         continue;
       }
 
-      if (shopName && shopName.includes(sellerName)) {
-        results.push({ seller: shopName, price: price });
+      if (storeName && storeName.includes(shopName)) {
+        resultsArray.push({ seller: storeName, price: offerPrice });
       }
     }
-    return results;
-  }, sellerName);
+    return resultsArray;
+  }, shopName);
 
   await browser.close();
-  return offers;
+  return offersList;
 }
-
-
-  await browser.close();
-  return offers;
-}
-
-app.get('/scrape', async (req, res) => {
-  const url = req.query.url;
-  const store = req.query.store;
-  const article = req.query.article;
-
-  let result;
-  if (store === 'Lidl') {
-    result = await scrapeLidl(url, article);
-  } else if (store === 'Netto') {
-    result = await scrapeNetto(url);
-  } else if (store === 'Idealo') {
-    const seller = decodeURIComponent(req.query.seller);
-    result = await scrapeIdealo(url, seller);
-  } else {
-    return res.status(400).send({ error: 'Invalid store type' });
-  }
-
-  res.send(result);
-});
-
-app.get('/', (req, res) => res.send('Scraping service is running.'));
-
-app.listen(port, () => {
-  console.log(`Scraping service listening at http://localhost:${port}`);
-});
